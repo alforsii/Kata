f73731456a43d0c2df2aa808fb860a568bd6cccf
Ashrafzhon Kurbonaliev <alforsii@gmail.com>
2020-02-29 11:47:07 -0500

    done

--- extractDomain/index.js
+++ extractDomain/index.js
@@ -1,3 +1,4 @@
+// https://www.codewars.com/kata/514a024011ea4fb54200004b/train/javascript
 // Extract the domain name from a URL
 // Write a function that when given a URL as a string, parses out just the domain name and returns it as a string. For example:
 
