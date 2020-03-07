b8ab6c70914fd93f9d0cf1419d4b0657132fd594
Ashrafzhon Kurbonaliev <alforsii@gmail.com>
2020-03-07 01:31:40 -0500

    update my solution

--- mostPopularActor/index.js
+++ mostPopularActor/index.js
@@ -8,17 +8,28 @@ var americanActors = [
   { name: 'Julia Roberts', popularityScore: 88 },
 ];
 // console.log(mostPopularActor(americanActors)); // => "Jack Nicholson"
+//1. Solution - 1
 function mostPopularActor(actors) {
-  let popScore = actors[0].popularityScore;
-
-  for (let i = 0; i < actors.length; i++) {
-    if (popScore < actors[i].popularityScore)
-      popScore = actors[i].popularityScore;
-  }
-  let popActor = actors.filter(actor => {
-    return actor.popularityScore === popScore;
-  });
-  console.log('Output for: mostPopularActor -> popActor', popActor);
+  let maxScore = actors.reduce(
+    (max, val) => Math.max(max, val.popularityScore),
+    actors[0].popularityScore
+  );
+  return actors.filter(actor => actor.popularityScore == maxScore)[0].name;
 }
 
+//2. Solution - 2
+// function mostPopularActor(actors) {
+//   let popScore = actors[0].popularityScore;
+
+//   for (let i = 0; i < actors.length; i++) {
+//     if (popScore < actors[i].popularityScore)
+//       popScore = actors[i].popularityScore;
+//   }
+//   let popActor = actors.filter(actor => {
+//     return actor.popularityScore === popScore;
+//   });
+//   console.log('Output for: mostPopularActor -> popActor', popActor);
+// }
+
 mostPopularActor(americanActors);
+console.log(mostPopularActor(americanActors));
