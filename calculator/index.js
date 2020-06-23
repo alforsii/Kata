d1a9d389c5638b92199a49c297ef60b3462e892d
Ashrafzhon Kurbonaliev <alforsii@gmail.com>
2020-06-23 13:35:27 -0400

    add url for kata

--- calculator/index.js
+++ calculator/index.js
@@ -1,3 +1,4 @@
+//https://www.codewars.com/kata/5235c913397cbf2508000048/train/javascript
 // Create a simple calculator that given a string of operators (), +, -, *, / and numbers separated by spaces returns the value of that expression
 
 // Example:
